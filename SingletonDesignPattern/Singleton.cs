4be1c3f22d18c60518ef32a97a463e829c1db7a8
redamabrouk <reda3032@gmail.com>
2019-10-22 03:01:34 +0200

    Singleton design pattern

---
+++ SingletonDesignPattern/Singleton.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CreationalDesignPatterns
+{
+    /// <summary>
+
+    /// The 'Singleton' class
+
+    /// </summary>
+
+    class Singleton
+    {
+        private static Singleton _instance;
+
+        // Constructor is 'protected' can be inherited
+
+        protected Singleton()
+        {
+        }
+
+        public static Singleton GetInstance()
+        {
+
+
+            if (_instance == null)
+            {
+                _instance = new Singleton();
+            }
+
+            return _instance;
+        }
+    }
+}
