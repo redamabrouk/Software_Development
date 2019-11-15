924447327e99c65cd0e3eed1f89451324a431a9b
Dr. Reda M. Hussien <reda3032@gmail.com>
2019-11-15 15:01:20 +0200

    Update readme.cmd

--- ToDoList/readme.cmd
+++ ToDoList/readme.cmd
@@ -5,4 +5,4 @@ Bridge pattern would the most appropriate to be used in this case. The abstracti
 presentations (listed items, items with bullets, numbered items) while the implementations cab
 be the way how the list are sorted (unordered list, a due date ordered list or a priority based
 ordered task list).
-<img src="https://github.com/redamabrouk/Software_Development/blob/master/ToDoList/To-Do-List.png" alt="Class Diagram"/>
+<img src="/ToDoList/To-Do-List.png" alt="Class Diagram"/>
